79347dbdc0e664d9d696f6c1ea172768654239d1
houfang <707980114@qq.com>
2015-04-28 16:17:38 +0800

    首页

--- app/src/main/java/com/xiyouji/xiyouji/MineFragment.java
+++ app/src/main/java/com/xiyouji/xiyouji/MineFragment.java
@@ -1,24 +1,20 @@
 package com.xiyouji.xiyouji;
 
-import android.app.Activity;
-import android.net.Uri;
+/**
+ * Created by houfang on 15/4/28.
+ */
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-/**
- *
- * Created by houfang on 15/4/26.
- *
- */
-public class HomeFragment extends Fragment {
+public class MineFragment extends Fragment
+{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // TODO Auto-generated method stub
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_mine, container, false);
     }
 }
