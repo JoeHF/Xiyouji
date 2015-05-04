a3e950728329be5ad7ddb4b576de8253f4c90e80
HuangChaoChao <1401214379@pku.edu.cn>
2015-05-04 12:01:05 +0800

    车辆信息，车辆位置,颜色2

---
+++ app/src/main/java/com/xiyouji/xiyouji/CarInfoActivity.java
@@ -0,0 +1,17 @@
+package com.xiyouji.xiyouji;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by houfang on 2015/4/29.
+ */
+public class CarInfoActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.information_car);
+    }
+
+
+}
---
+++ app/src/main/java/com/xiyouji/xiyouji/CarlocActivity.java
@@ -0,0 +1,17 @@
+package com.xiyouji.xiyouji;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by houfang on 2015/4/29.
+ */
+public class CarlocActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.location_detail);
+    }
+
+
+}
