9c6be01a4c5c06ef02e256bd9a08b0c1d3b23159
HuangChaoChao <1401214379@pku.edu.cn>
2015-05-05 22:01:53 +0800

    车辆信息，车辆位置,颜色2

--- app/src/main/java/com/xiyouji/xiyouji/CarInfoActivity.java
+++ app/src/main/java/com/xiyouji/xiyouji/CarInfoActivity.java
@@ -1,7 +1,9 @@
 package com.xiyouji.xiyouji;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by houfang on 2015/4/29.
@@ -12,6 +14,13 @@ public class CarInfoActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.information_car);
     }
-
+    public void click_to_city(View v)
+    {
+        Intent intent = new Intent();
+        intent.setClass(this, NumberCheckActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.push_left_in,
+                R.anim.push_left_out	);
+    }
 
 }
